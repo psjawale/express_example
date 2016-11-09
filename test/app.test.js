a5e229bd44c816d229a758584f60c42dac50a458
psjawale <psjawale@ncsu.edu>
2016-11-09 14:28:05 -0500

    trying xpush

--- test/app.test.js
+++ test/app.test.js
@@ -4,19 +4,9 @@ var app = require('../app'),
 
 describe('GET /', function(){
   before(function() {
-      var port = '3000';
+      var port = '3001';
       app.set('port', port);
       var server = http.createServer(app);
       server.listen(port);
   });
-
-  it('should return a 200 status code', function (done){
-    http.get({ host: '0.0.0.0', port: 3000 }, function(res) {
-      assert.deepEqual(res.statusCode, 200);
-      done();
-    }).on('error', function(e) {
-      throw new Error(e);
-    });
-  });
-
 });
---
+++ update-server.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+echo ran this script after push automatically
